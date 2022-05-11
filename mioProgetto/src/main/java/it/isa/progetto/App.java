0f9240f5647aeb6aeb330ddcfba7e24fb5651559
AlbreschiDavide <albreschi1997@gmail.com>
2022-05-11 11:05:07 +0200

    Aggiornamento del file

--- mioProgetto/src/main/java/it/isa/progetto/App.java
+++ mioProgetto/src/main/java/it/isa/progetto/App.java
@@ -1,13 +1,35 @@
 package it.isa.progetto;
 
-/**
- * Hello world!
- *
- */
+import it.isa.progetto.Insert;
+import java.sql.*;
+import javax.sql.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Database select = new Database();
+        //Database from = new Database();
+        //Database where = new Database();
+        System.out.println("Hello Word");
+
+        String select = "SELECT *";
+        String from = "FROM CLIENTE";
+        String where = "";
+        String query = select+" "+from+" "+ where+";" ;
+        
+		//Database query = new Database();
+        //query.createQuery(select,from,where);
+
+        System.out.println("Query: "+ select);
+        System.out.println("Query: "+ from);
+        System.out.println("Query: "+ where);
+        System.out.println("Query: "+ query);
+        
+        //Insert.InsertRow(query);
+        Update.update(query);
+		
+       
+        	
     }
 }

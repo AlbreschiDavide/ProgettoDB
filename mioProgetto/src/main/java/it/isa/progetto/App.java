921a349b1af0f1b75aa3246039d6d9035283a0ab
AlbreschiDavide <albreschi1997@gmail.com>
2022-05-12 16:20:19 +0200

    Interfaccia da terminale aggiornata

--- mioProgetto/src/main/java/it/isa/progetto/App.java
+++ mioProgetto/src/main/java/it/isa/progetto/App.java
@@ -1,6 +1,10 @@
 package it.isa.progetto;
 
 import it.isa.progetto.Insert;
+import it.isa.progetto.Update;
+import it.isa.progetto.Join;
+
+import java.util.*;
 import java.sql.*;
 import javax.sql.*;
 
@@ -11,25 +15,50 @@ public class App
         //Database select = new Database();
         //Database from = new Database();
         //Database where = new Database();
-        System.out.println("Hello Word");
+        System.out.println("Ciao!");
 
         String select = "SELECT *";
         String from = "FROM CLIENTE";
         String where = "";
-        String query = select+" "+from+" "+ where+";" ;
+        String query= select+" "+from+" "+ where+";" ;
         
 		//Database query = new Database();
         //query.createQuery(select,from,where);
 
-        System.out.println("Query: "+ select);
-        System.out.println("Query: "+ from);
-        System.out.println("Query: "+ where);
-        System.out.println("Query: "+ query);
+        //System.out.println("Query: "+ select);
+        //System.out.println("Query: "+ from);
+        //System.out.println("Query: "+ where);
+        //System.out.println("Query: "+ query);
         
-        //Insert.InsertRow(query);
-        Update.update(query);
-		
-       
-        	
+		System.out.println("Che operazione si vuole eseguire sul DB:\n(Inserire il corrispettivo numero)\n"
+                                +"1. Insert\n2. Delete\n"
+                                +"3. Update\n4. Join");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for input: ");
+        try{
+            int num = sc.nextInt();
+            System.out.println(num);
+            switch(num){
+                case 1:
+                    Insert.InsertRow(query);
+                    break;
+                case 2:
+                    //Delete.(query);
+                    break;
+                case 3:
+                    Update.UpdateColumn(query);
+                    break;
+                case 4:
+                    Join.JoinTables(query);
+                    break;
+                default:
+                    System.out.println("Non è stato inserito uno dei numeri richiesti...\nTermino...");
+                    System.exit(0);
+            }
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    	
     }
+
 }
